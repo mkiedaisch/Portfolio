9b1617c0a921a0ba4551d2686da26673d6535766
mkiedaisch <matthewkiedaisch19@gmail.com>
2025-06-20 00:56:56 -0500

    Update 3_Resume.py

--- streamlit_portfolio/pages/3_Resume.py
+++ streamlit_portfolio/pages/3_Resume.py
@@ -2,12 +2,10 @@ import streamlit as st
 
 st.title("Resume")
 
-with open("resume.pdf", "rb") as file:
-    st.download_button(
-        label="📥 Download Resume",
-        data=file,
-        file_name="resume.pdf",
-        mime="application/pdf"
-    )
+with open("../resume.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
 
-st.write("You can also reach me on LinkedIn or via email.")
+st.download_button(label="📄 Download Resume",
+                   data=PDFbyte,
+                   file_name="Matthew_Kiedaisch_Resume.pdf",
+                   mime="application/pdf")
